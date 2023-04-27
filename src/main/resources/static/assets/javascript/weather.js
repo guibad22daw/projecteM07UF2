d35a80e984adbac4e73ab6b87d1974ce1a8c5a10
guibad22daw <15587039.clot@fje.edu>
2023-04-27 19:27:05 +0200

    Barra de cerca completada

--- src/main/resources/static/assets/javascript/weather.js
+++ src/main/resources/static/assets/javascript/weather.js
@@ -1,18 +1,79 @@
 window.onload = function () {
-    var weather;
-    var url = "https://api.open-meteo.com/v1/forecast?latitude=41.39&longitude=2.16&daily=weathercode,temperature_2m_max&current_weather=true&timezone=auto";
-    var request = new XMLHttpRequest();
-    request.open('GET', url);
-    request.responseType = 'json';
-    request.send();
-    request.onload = function () {
-        weather = request.response;
-        console.log(weather);
-        document.getElementById("weather").innerHTML = weather.current_weather.weathercode;
+    const suggestions = document.getElementById("suggestions");
+    const cerca = document.getElementById('cerca');
 
-        weather.current_weather.is_day ? (
-            document.getElementById("weather").innerHTML = `<img src="assets/img/weather-icons/weathercode-${weather.current_weather.weathercode}.svg" alt="weather">`
-            ) : (console.log('second'));
-        document.getElementById("temperature").innerHTML = weather.current_weather.temperature;
+    document.onmousedown = function () {
+        const isClickInsideSuggestions = suggestions.contains(event.target);
+        const isClickInsideCerca= cerca.contains(event.target);
+      
+        if (!isClickInsideSuggestions && !isClickInsideCerca) {
+            suggestions.style.display = 'none';
+            cerca.style.borderRadius = "20px"; 
+        }
+    };
+
+    cerca.onclick = async function () {
+        suggestions.style.display = "block";
+        cerca.style.borderBottomLeftRadius = "10px"; 
+        cerca.style.borderBottomRightRadius = "10px";
+    }
+
+    cerca.onkeyup = async function () {
+        const value = this.value;
+        if (!value) {
+            while (suggestions.firstChild) {
+                suggestions.removeChild(suggestions.firstChild);
+            }
+            return;
+        }
+
+        try {
+            const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${value}`);
+
+            if (response.ok) {
+                const data = await response.json();
+                const resultats = data.results;
+
+                while (suggestions.firstChild) {
+                    suggestions.removeChild(suggestions.firstChild);
+                }
+
+                resultats.forEach(suggestion => {
+                    console.log('suggestion', suggestion);
+                    const localitzacio = document.createElement("li");
+                    localitzacio.value = `${suggestion.name}`;
+                    localitzacio.innerHTML = `${suggestion.name}, ${suggestion.admin1}, ${suggestion.country}`;
+                    localitzacio.onclick = () => {
+                        suggestions.style.display = 'none';
+                        cerca.style.borderRadius = "20px";
+                        cerca.value = `${suggestion.name}, ${suggestion.admin1}, ${suggestion.country}`;
+                        getWeather(suggestion.latitude, suggestion.longitude);
+                    };
+                    suggestions.appendChild(localitzacio);
+                });
+            }
+        } catch (err) {
+            console.log('err :>> ', err);
+        }
+    };
+
+    document.getElementById('formCerca').onsubmit = async function(e){
+        e.preventDefault();
+        let ciutat = cerca.value;
+        const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${ciutat}`);
+        const data = await response.json();
+        console.log(data);
+        getWeather(data.results[0].latitude, data.results[0].longitude);
+    };
+
+    async function getWeather(lat, lon) {
+        const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=weathercode,temperature_2m_max&current_weather=true&timezone=auto`);
+        const data = await response.json();
+        console.log(data);
+        document.getElementById("weather").innerHTML = data.current_weather.weathercode;
+        data.current_weather.is_day ? (
+            document.getElementById("weather").innerHTML = `<img src="assets/img/weather-icons/weathercode-${data.current_weather.weathercode}.svg" alt="weather">`
+            ) : (document.getElementById("weather").innerHTML = `<img src="assets/img/weather-icons/night/weathercode-${data.current_weather.weathercode}.svg" alt="weather">`);
+        document.getElementById("temperature").innerHTML = data.current_weather.temperature;
     }
 }
